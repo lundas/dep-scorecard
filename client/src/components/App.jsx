1df43c1d95a57cc43fd62b3ed6bbe11bf164a9e2
Andrew Lund <andrew.s.lund@gmail.com>
2023-06-06 09:10:46 -0700

    made adjustments for htmlwebpackplugin

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="body" role="application">
       <div className="site-header-container">
-        <h1 className="site-header">Hello, World</h1>
+        <h1 className="text-3xl font-bold underline hover:bg-sky-700">Hello, World</h1>
       </div>
     </div>
   );
