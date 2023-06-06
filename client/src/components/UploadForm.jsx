be25a424b012e0deb1b55d879d12bd3905fc4dde
Andrew Lund <andrew.s.lund@gmail.com>
2023-06-06 13:39:14 -0700

    post route partially implemented

--- client/src/components/UploadForm.jsx
+++ client/src/components/UploadForm.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
 export default function UploadForm() {
   function handleSubmit(files) {
-    console.log('Files submitted', files);
+    const form = new FormData();
+    form.append('file', files[0]);
+    return axios.post('/api', form, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
   }
   return (
-    <form>
-      <input type="file" />
+    <form encType="multipart/form-data">
+      <input type="file" name="file" />
       <input
         type="submit"
         onClick={(e) => {
           e.preventDefault();
           e.stopPropagation();
           const { files } = document.querySelector('input[type=file]');
-          handleSubmit(files);
+          handleSubmit(files)
+            .then((res) => console.log('Submit res', res))
+            .catch((err) => console.log('Submit Err', err));
         }}
       />
     </form>
--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const multer = require('multer');
+
+const upload = multer();
 
 const app = express();
 
@@ -11,6 +14,12 @@ app.use(morgan('dev'));
 app.use(cors());
 
 const port = process.env.PORT || 3000;
+
+app.post('/api', upload.single('file'), (req, res) => {
+  const fileContents = JSON.parse(req.file.buffer.toString());
+  console.log('Dependencies', fileContents.dependencies);
+  res.status(200).send('got your request, bruh');
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
