5dc3a81814d9a4251f53ab935d5ed308bdd80ced
Andrew Lund <andrew.s.lund@gmail.com>
2023-06-06 20:47:22 -0700

    database built; inserts working

---
+++ db/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB);
+
+const dependecySchema = new mongoose.Schema({
+  name: String,
+  version: String,
+});
+
+const packageSchema = new mongoose.Schema({
+  name: String,
+  version: String,
+  repository: String,
+  homepage: String,
+  dependencies: [dependecySchema],
+  devDependencies: [dependecySchema],
+});
+
+const Package = mongoose.model('Package', packageSchema);
+
+module.exports = Package;
