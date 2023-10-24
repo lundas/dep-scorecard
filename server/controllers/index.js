426b226a6a6c668dbe00c8d38c85486432c6a7ca
Andrew Lund <andrew.s.lund@gmail.com>
2023-10-24 16:34:30 -0700

    model an controller working with ossf api & deps.dev api

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -23,25 +23,31 @@ exports.processFile = (req, res) => {
   // const depArray = Promise.all([depRows, devDepRows]);
 
   const allDeps = { ...dependencies, ...devDependencies };
-  const depArray = exports.buildDependecyArray(allDeps).then((results) => results.forEach((r) => console.log(r.data)));
+  const depArray = exports.buildDependecyArray(allDeps)
+    .then((results) => (
+      results.map((result) => (
+        [
+          result && result[0] ? result[0].data : null,
+          result && result[1] ? result[1].data : null,
+        ]
+      ))
+    ));
 
-  res.sendStatus(200);
-
-  // depArray
-  //   .then((results) => {
-  //     res.status(201).json({ dependencies: results, devDependencies: [] });
-  //   })
-  //   .catch((err) => {
-  //     console.log('Error assembling dependency array:', err);
-  //     res.sendStatus(500);
-  //   });
+  depArray
+    .then((results) => {
+      res.status(201).json(results);
+    })
+    .catch((err) => {
+      console.log('Error assembling dependency array:', err);
+      res.sendStatus(500);
+    });
 };
 
 exports.buildDependecyArray = (dependencyObj) => {
   const pkgs = Object.keys(dependencyObj);
   const data = pkgs.map((pkg) => (
     // models.getProjectData(pkg, dependencyObj[pkg].slice(1))
-    models.getVersionData(pkg, dependencyObj[pkg].slice(1))
+    models.getProjectData(pkg, dependencyObj[pkg].slice(1))
   ));
   return Promise.all(data);
 };
--- server/models/index.js
+++ server/models/index.js
@@ -13,31 +13,22 @@ exports.getVersionData = (name, version, system = 'npm') => (
   )
 );
 
-exports.getProjectData = (name, version, system = 'npm') => exports.getVersionData(name, version, system)
-  .then((results) => {
-    const sourceRepo = results.data.links.filter((link) => link.label === 'SOURCE_REPO')[0].url;
+exports.getScorecard = (url) => (
+  axios.get(
+    `${process.env.OSSF_URL}/projects/${url}`,
+  )
+);
+
+exports.getProjectData = async (name, version, system = 'npm') => {
+  try {
+    const versionReq = await exports.getVersionData(name, version, system);
+    const sourceRepo = versionReq.data.links.filter((link) => link.label === 'SOURCE_REPO')[0].url;
     const sourceUrl = new URL(sourceRepo);
     const projectKeyId = `${sourceUrl.hostname}${sourceUrl.pathname.split('.')[0]}`;
-    return [
-      results,
-      axios.get(`${process.env.OSSF_URL}/projects/${projectKeyId}`)
-        .catch((err) => {
-          console.log(`Error getting OSSF Score for ${name}`);
-          if (err.response) {
-            console.log('*** Response Data: ', err.response.data);
-            console.log('*** Response Status: ', err.response.status);
-            console.log('*** Response Headers: ', err.response.headers);
-          } else if (err.request) {
-            console.log('*** Response Request: ', err.request);
-          } else {
-            console.log('*** Error', err.message);
-          }
-          console.log('*** Error Config', err.config);
-        }),
-    ];
-  })
-  .catch((err) => {
-    console.log(`Error getting Version Data for ${name}`);
+    const OssfReq = await exports.getScorecard(projectKeyId);
+    return [versionReq, OssfReq];
+  } catch (err) {
+    console.log(`Error getting Project Data for ${name}`);
     if (err.response) {
       console.log('*** Response Data: ', err.response.data);
       console.log('*** Response Status: ', err.response.status);
@@ -48,7 +39,45 @@ exports.getProjectData = (name, version, system = 'npm') => exports.getVersionDa
       console.log('*** Error', err.message);
     }
     console.log('*** Error Config', err.config);
-  });
+  }
+};
+
+// exports.getProjectData = (name, version, system = 'npm') => exports.getVersionData(name, version, system)
+//   .then((results) => {
+//     const sourceRepo = results.data.links.filter((link) => link.label === 'SOURCE_REPO')[0].url;
+//     const sourceUrl = new URL(sourceRepo);
+//     const projectKeyId = `${sourceUrl.hostname}${sourceUrl.pathname.split('.')[0]}`;
+//     return [
+//       results,
+//       axios.get(`${process.env.OSSF_URL}/projects/${projectKeyId}`)
+//         .catch((err) => {
+//           console.log(`Error getting OSSF Score for ${name}`);
+//           if (err.response) {
+//             console.log('*** Response Data: ', err.response.data);
+//             console.log('*** Response Status: ', err.response.status);
+//             console.log('*** Response Headers: ', err.response.headers);
+//           } else if (err.request) {
+//             console.log('*** Response Request: ', err.request);
+//           } else {
+//             console.log('*** Error', err.message);
+//           }
+//           console.log('*** Error Config', err.config);
+//         }),
+//     ];
+//   })
+//   .catch((err) => {
+//     console.log(`Error getting Version Data for ${name}`);
+//     if (err.response) {
+//       console.log('*** Response Data: ', err.response.data);
+//       console.log('*** Response Status: ', err.response.status);
+//       console.log('*** Response Headers: ', err.response.headers);
+//     } else if (err.request) {
+//       console.log('*** Response Request: ', err.request);
+//     } else {
+//       console.log('*** Error', err.message);
+//     }
+//     console.log('*** Error Config', err.config);
+//   });
 
 exports.insertPackage = (name, version, repository, homepage, dependencies, devDependencies) => {
   const depArray = Object.keys(dependencies).map((key) => (
