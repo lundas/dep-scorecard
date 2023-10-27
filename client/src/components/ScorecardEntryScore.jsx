fd7bab9a647d876640f925423825f6b51bf4c129
Andrew Lund <andrew.s.lund@gmail.com>
2023-10-27 15:21:07 -0700

    added logic to account for packages with no scorecards

--- client/src/components/ScorecardEntryScore.jsx
+++ client/src/components/ScorecardEntryScore.jsx
@@ -4,7 +4,7 @@ export default function ScorecardEntryScore({ dependency }) {
   return (
     <div className="flex flex-col flex-wrap justify-evenly">
       <h2 className="text-3xl font-diph text-center">OSSF Score</h2>
-      <div>{dependency.scorecard.score}</div>
+      <div>{dependency.scorecard ? dependency.scorecard.score : 'N/A'}</div>
     </div>
   );
 }
